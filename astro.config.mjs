1627239acb211735d4efd556bac257d0a46965f8
Yurb <yavurb@gmail.com>
2024-08-29 00:59:03 -0400

    :bug: fix: add broadcasts page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import nodejs from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
