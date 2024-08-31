bde2f5d8a2aab417930c9c58416459ac8cd68138
Yurb <yavurb@gmail.com>
2024-08-31 12:52:20 -0400

    :recycle: refactor: define action to get broadcasts

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,36 +1,60 @@
+import type Broadcast from "@/interfaces/broadcast";
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
 
 const SERVER_URL = import.meta.env.BACKEND_URL;
 
-export const server = {
-  createBroadcast: defineAction({
-    input: z.object({
-      title: z.string({ required_error: "Broadcast's title is required" }).min(3),
-      sdp: z.string(),
-    }),
-    handler: async (input) => {
-      const response = await fetch(`${SERVER_URL}/broadcasts`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-        body: JSON.stringify({
-          title: input.title,
-          sdp: input.sdp,
-        }),
-      });
-
-      console.log("response", response);
-
-      if (response.ok) {
-        const broadcast = (await response.json()) as { sdp: string };
-
-        return broadcast.sdp;
-      }
-
-      return null;
-    },
+const createBroadcast = defineAction({
+  input: z.object({
+    title: z.string({ required_error: "Broadcast's title is required" }).min(3),
+    sdp: z.string(),
   }),
+  handler: async (input) => {
+    const data = {
+      title: input.title,
+      sdp: input.sdp,
+    }
+    console.log("data", data);
+    const response = await fetch(`${SERVER_URL}/broadcasts`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+
+    if (response.ok) {
+      const broadcast = (await response.json()) as { sdp: string };
+      return broadcast.sdp;
+    }
+
+    return null;
+  },
+});
+
+const getBroadcasts = defineAction({
+  handler: async () => {
+    const response = await fetch(`${SERVER_URL}/broadcasts`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+
+    // TODO: Handle error
+    if (response.ok) {
+      const broadcast = (await response.json()) as { broadcasts: Broadcast[] };
+      return broadcast.broadcasts;
+    }
+
+    return [];
+  },
+});
+
+export const server = {
+  createBroadcast,
+  getBroadcasts,
 }
